ec1759e95ab6ac64b8f1b56e4898ed204321077c
Martin Aracama <martinaracama@gmail.com>
2022-08-21 02:28:05 -0300

    Se creo carpeta components y archivo header.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import { Header } from "./components/header"
+
+
 
 const App = () => {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='Parrafo'>
-          Próximamente todo lo que necesitas para cocinar<br></br> 
-          lo encontraras en este sitio
-        </p>
-        <a
-          className="App-link"
-          href="https://google.com.ar"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          COCINANDO
-        </a>
-      </header>
+    <div>
+      <Header/>
+      
     </div>
   );
 }
