fe6554c05a1739d897dd78376abafe008cbb85d9
Martin Aracama <martinaracama@gmail.com>
2022-08-27 17:18:29 -0300

    se elimina carpeta main y ajustes varios

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import "./App.css";
 import { Header } from "./components/header/header"
 import { Footer } from "./components/footer/footer"
-import {Main} from "./components/main/main"
+
 
 const App = () => {
 
   return (
     <div>
       <Header/>
-      <Main/>
+      
       <Footer/>
     </div>
   );
