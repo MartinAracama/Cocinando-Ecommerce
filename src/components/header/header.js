fad74bf8c41a33a665f62e76280d877eef15ad82
Martin Aracama <martinaracama@gmail.com>
2022-08-28 13:12:14 -0300

    se agrega logo cart al navbar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,8 +6,8 @@ export const Header = () => {
     return (
         <header className="header">
             <h1 className="title">COCINANDO</h1>
-            
             <nav className="navbar">
+            <img className="cart" src="./CartLogo.png" alt="logo carrito compras"></img>
                 <a className="navbar-link">Inicio</a>
                 <a className="navbar-link">Categorias</a>
                 <a className="navbar-link">Ofertas</a>
