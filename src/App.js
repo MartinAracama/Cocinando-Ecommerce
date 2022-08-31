c58cac41185b461524a4906f8646bc5ff146c643
Martin Aracama <martinaracama@gmail.com>
2022-08-30 22:52:37 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Header } from "./components/header/header"
 import { Footer } from "./components/footer/footer"
 import { ItemListContainer } from "./components/itemListContainer/itemListContainer"
 import Contador from "./components/Contador/Contador"
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const App = () => {
   
