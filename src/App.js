1c3fc6cb76c487de2e37e4420bea078cc83b6ad7
Martin Aracama <martinaracama@gmail.com>
2022-08-31 21:36:37 -0300

    BrouserRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
-import { Header } from "./components/header/header"
-import { Footer } from "./components/footer/footer"
-import { ItemListContainer } from "./components/itemListContainer/itemListContainer"
-
+import { Header } from "./components/header/header";
+import { Footer } from "./components/footer/footer";
+import { ItemListContainer } from "./components/itemListContainer/itemListContainer";
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
 
 const App = () => {
   
   return (
+    
     <div>
-      <Header />
-      <ItemListContainer/>
-      <Footer/>
+      <BrowserRouter>
+        
+        <Header />
+          
+        <Routes>
+          <Route path="/" element={ <ItemListContainer/> } />
+          <Route path="*" element={ <Navigate to="/"/> } />
+        </Routes>
+      
+        
+        <Footer/>
+      
+      </BrowserRouter>
+
     </div>
   );
 }
