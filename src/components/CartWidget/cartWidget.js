eda34d333a85d3f7fa68b63295c5cbf142a1cbc9
Martin Aracama <martinaracama@gmail.com>
2022-09-26 00:12:08 -0300

    ajustes de estilo

--- src/components/CartWidget/cartWidget.js
+++ src/components/CartWidget/cartWidget.js
@@ -9,7 +9,7 @@ export const CartWidget = () => {
   const { cartQ } = useContext(CartContext)
 
   return (
-    <Link to="/cart">
+    <Link to="/cart" className='cartLogo'>
       <div className='cart'>
         <FaShoppingCart style={{color: "#c9910e", fontSize: "40px"}} />
         <span className='cartQ'>{cartQ()}</span>
